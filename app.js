9fe65972aab99912237657d11d66427fbb41ebfa
park jinsung <jinsung@parks-MacBook-Air.local>
2023-04-30 18:22:03 +0900

    a

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const OPENAI_API_KEY = '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';
+
 const express = require('express');
 const http = require('http');
 const path = require('path');
@@ -7,6 +9,12 @@ const indexRouter = require('./routes/index');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+// const openai = require('openai');
+// openai.apiKey=OPENAI_API_KEY;
+
+
+// const { OpenAI } = require('openai');
+// const openai = new OpenAI(OPENAI_API_KEY);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,20 +29,37 @@ app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
+
 // Socket.io
 io.on('connection', socket => {
   console.log('New WebSocket connection');
 
-  socket.emit('message', 'Welcome to JoyBot');
-
+  socket.emit('message', 'Hello, how can I help you?');
   socket.on('chatMessage', message => {
-    io.emit('message', message);
+    const model = 'text-davinci-002';
+    const maxTokens = 5;
+
+    console.log(`receive : ${message}`);
+    io.emit('message', `receive : ${message}`);
+
+    // openai.complete({
+    //   engine: model,
+    //   prompt: message,
+    //   maxTokens: maxTokens,
+    // }).then((response) => {
+    //   console.log(`response : ${response.choices[0].text}`);
+    //   io.emit('message', `${response.choices[0].text}`);
+    // }).catch((error) => {
+    //   console.error(error);
+    //   io.emit('message', `error: '${error}'`);
+    // });
   });
 
   socket.on('disconnect', () => {
     console.log('WebSocket disconnected');
   });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
---
+++ public/js/chat.js
@@ -0,0 +1,41 @@
+const chatForm = document.querySelector('.chat-input');
+const chatMessages = document.querySelector('.chat-content');
+
+const socket = io();
+
+socket.on('message', message => {
+  outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Get message text
+  const msg = e.target.elements.message.value;
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Add message to chat content
+  outputMessage(msg, true);
+
+  // Clear input
+  e.target.elements.message.value = '';
+  e.target.elements.message.focus();
+});
+
+
+
+// Output message to DOM
+function outputMessage(message, isSent) {
+  const div = document.createElement('div');
+  div.classList.add('chat-bubble');
+  if (isSent) {
+    div.classList.add('chat-bubble-response');
+  }
+  div.innerHTML = `<p class="chat-message">${message}</p>`;
+  chatMessages.appendChild(div);
+}
