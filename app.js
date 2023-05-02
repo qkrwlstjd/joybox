a75a0de0765891128a733598c9f95402a9a0b55c
park jinsung <jinsung@parks-MacBook-Air.local>
2023-05-02 23:06:51 +0900

    db delete

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/joybox.im/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/joybox.im/fullchain.pem"),
+};
+
 const express = require("express");
-const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
 const indexRouter = require("./routes/index");
 const AIAssistant = require("./generate");
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 const io = socketio(server);
 const bodyParser = require("body-parser");
 
@@ -25,7 +32,7 @@ app.use((req, res) => {
   res.status(404).send("Page not found");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
